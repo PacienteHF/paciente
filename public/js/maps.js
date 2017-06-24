4ba7b467d338af172d7c9fadd0b8af09817b94da
yasser nascimento <yassern@protonmail.com>
2017-06-24 11:47:41 -0300

    mapa estilizado

--- public/js/maps.js
+++ public/js/maps.js
@@ -5,8 +5,16 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -7.2251168, lng: -35.8975412},
-      zoom: 10
+      zoom: 7
     });
+
+    var style =
+    [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+
+    var styledMapType = new google.maps.StyledMapType(style, {name: 'Styled Map'});
+
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 
     var markers = locations.map(function(location) {
       return new google.maps.Marker({
