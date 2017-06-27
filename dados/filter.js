b9f0b85e6cad51b1afefe87b3acbfab948c5ef32
yasser nascimento <yassern@protonmail.com>
2017-06-26 21:44:53 -0300

    removido dos dados validos os estabelecimentos que não são pontos de atendimento

--- dados/filter.js
+++ dados/filter.js
@@ -1,6 +1,8 @@
 var jsonfile = require('jsonfile');
+var dadosValidos = require('./dados-validos');
 var path = 'dados-completos.json';
 
+/*
 jsonfile.readFile(path, function(err, estabelecimentos) {
   // Filtra os estabelecimentos que atendem SUS e possuem dados lat/long completos
   var validos = estabelecimentos.filter(function(estabelecimento) {
@@ -13,4 +15,29 @@ jsonfile.readFile(path, function(err, estabelecimentos) {
   jsonfile.writeFile(pathFiltrados, validos, function (err) {
     console.error(err);
   });
+});
+*/
+
+// Filtra dos estabelecimentos válidos os estabelecimentos que não são centrais de atendimento
+var listaInvalidos = ["SAMU", "SECRETARIA", "ACADEMIA", "VIGILANCIA", "FARMACIA",
+                      "LABORATORIO", "SINDICATO", "PENITENCIARIA", "PRESIDIO", "REGULACAO"];
+
+function geraRegExp(list) {
+  var strRegExp = '';
+  var last = list.pop();
+  list.forEach(function(item) {
+    strRegExp += ('\\b' + item + '\\b' + '\|');
+  });
+  strRegExp += '\\b' + last + '\\b';
+  return new RegExp(strRegExp);
+}
+
+var regExp = geraRegExp(listaInvalidos);
+var validos = dadosValidos.filter(function(estabelecimento) {
+  return !regExp.test(estabelecimento['NOME FANTASIA']);
+});
+
+var pathFiltrados = 'dados-validos.json';
+jsonfile.writeFile(pathFiltrados, validos, function (err) {
+  console.error(err);
 });
