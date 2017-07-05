4a0f8b3ba1a97c01b5d632cd26c15f75c7f1ec2a
yasser nascimento <yassern@protonmail.com>
2017-07-05 16:47:31 -0300

    criado router estabelecimentos

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    path = require("path")
-    mongoose = require("mongoose")
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
-var Estabelecimento = require("./models/estabelecimento");
 require("./models/connect")();
-var root = path.resolve(".");
-var router = express.Router();
+
+var router = require('./routes/index');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,39 +17,11 @@ app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome/
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/autocomplete', express.static(__dirname + '/node_modules/devbridge-autocomplete/dist/'));
 
+app.use('/', router);
+
 app.get("/", function(req, res) {
   res.render('index');
 });
-
-app.get("/dados", function(req, res) {
-  Estabelecimento.find({}, function(err, estabelecimentos) {
-    res.json(estabelecimentos);
-  })
-});
-
-app.post("/avaliarEstrutura", function(req, res) {
-  var data = req.body;
-  var id = { id: Number(data.id) };
-  var avaliacao = { nota : Number(data.nota), comentario: data.problemaEstrutura };
-  var update = { $push: { "avaliacoes.infraestrutura" : avaliacao } };
-  var query = Estabelecimento.findOneAndUpdate(id, update, function(err) {
-    console.log(err);
-  });
-  res.redirect('/');
-});
-
-router.route('/:id').get(function(req, res) {
-  var id = req.path.substring(1); // remove / do path
-  var query = Estabelecimento.findOne({ id: id });
-  query.select('nome endereco id');
-  query.exec(function(err, local) {
-    res.render(root + '/views/local', { nome: local.nome,
-                                        endereco: local.endereco.logradouro,
-                                        id: local.id } );
-  });
-});
-
-app.use('/local', router);
 
 app.listen(3000, function() {
   console.log("Server Running!");
