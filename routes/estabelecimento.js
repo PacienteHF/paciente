35d09c192ff9378bda82742a239680bcddd4860b
yasser nascimento <yassern@protonmail.com>
2017-07-07 01:58:55 -0300

    rota api integrada com formulario estrutura-irregular

--- routes/estabelecimento.js
+++ routes/estabelecimento.js
@@ -13,11 +13,13 @@ router.route('/estabelecimentos/:id')
   .get(function(req, res) {
     var id = req.params.id;
     controller.get(id, function(estabelecimento) {
-      var data = { nome: estabelecimento.nome, endereco: estabelecimento.endereco.logradouro }
+      var data = { id: estabelecimento.id,
+                   nome: estabelecimento.nome,
+                   endereco: estabelecimento.endereco.logradouro }
       res.render('estabelecimento', data);
     });
   })
-  .put(function(req, res) {
+  .post(function(req, res) {
     var id = req.params.id;
     var avaliacao = req.body;
     controller.avalia(id, avaliacao, function(err) {
