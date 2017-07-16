e26c172ed578b12209939e8c4616fbd9e5a4022f
Léo Câmara <leo.camara@gmail.com>
2017-07-16 01:52:51 -0300

    add mongolab

--- config/db.js
+++ config/db.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect("mongodb://localhost/saude");
+    //mongoose.connect("mongodb://localhost/saude");
+    mongoose.connect("mongodb://pacientemaster:pacientehf@ds027145.mlab.com:27145/saude");
 
-  mongoose.connection.on('connected', function () {
-    console.log('mongo conectado');
-  });
-
-  mongoose.connection.on('disconnected', function () {
-    console.log('mongo desconectado');
-  });
-
-  mongoose.connection.on('error', function () {
-    console.log('erro na conexão com mongo');
-  });
-
-  process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-      console.log('bye mongo');
-      process.exit(0);
+    mongoose.connection.on('connected', function() {
+        console.log('mongo conectado');
     });
-  });
+
+    mongoose.connection.on('disconnected', function() {
+        console.log('mongo desconectado');
+    });
+
+    mongoose.connection.on('error', function() {
+        console.log('erro na conexão com mongo');
+    });
+
+    process.on('SIGINT', function() {
+        mongoose.connection.close(function() {
+            console.log('bye mongo');
+            process.exit(0);
+        });
+    });
 }
