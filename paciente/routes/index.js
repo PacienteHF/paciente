14af108c0a53af5de261e8bd92875379fce05acc
Léo Câmara <leo.camara@gmail.com>
2017-07-16 13:31:44 -0300

    pasta desnecessaria

--- paciente/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var estabelecimento = require('./estabelecimento');
-
-router.use('/estabelecimentos', estabelecimento);
-
-module.exports = router;
--- paciente/server.js
+++
@@ -1,6 +0,0 @@
-var app = require('./config/app');
-require("./config/db")();
-
-app.listen(3000, function() {
-  console.log("Server Running!");
-});
