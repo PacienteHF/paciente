d4848a21681b4f5d8bdb366128aac4e68ec99adf
Project-HAM <livia.carolline@nutes.uepb.edu.br>
2017-07-29 18:04:59 -0300

    Update index

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('./config/app');
 require("./config/db")();
 
-app.listen(3000, function() {
-  console.log("Server Running!");
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+    console.log("Server Running!");
 });
