42338e363383aac25f0282d055508059a97bc591
Léo Câmara <leo.camara@gmail.com>
2017-07-29 22:06:01 -0300

    conectado ao mLab

--- config/db.js
+++ config/db.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect("mongodb://localhost/saude");
+    //mongoose.connect("mongodb://localhost/saude");
+    mongoose.connect("mongodb://pacientehf:paciente@ds127783.mlab.com:27783/saude");
 
-  mongoose.connection.on('connected', function () {
-    console.log('mongo conectado');
-  });
-
-  mongoose.connection.on('disconnected', function () {
-    console.log('mongo desconectado');
-  });
-
-  mongoose.connection.on('error', function () {
-    console.log('erro na conexão com mongo');
-  });
-
-  process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-      console.log('bye mongo');
-      process.exit(0);
+    mongoose.connection.on('connected', function() {
+        console.log('mongo conectado');
     });
-  });
+
+    mongoose.connection.on('disconnected', function() {
+        console.log('mongo desconectado');
+    });
+
+    mongoose.connection.on('error', function() {
+        console.log('erro na conexão com mongo');
+    });
+
+    process.on('SIGINT', function() {
+        mongoose.connection.close(function() {
+            console.log('bye mongo');
+            process.exit(0);
+        });
+    });
 }
