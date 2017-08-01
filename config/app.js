e85dcf455310b88ef772b7ea1a5b84aee36d1da1
Léo Câmara <leo.camara@gmail.com>
2017-08-01 18:51:29 -0300

    atualizacao de textos

--- config/app.js
+++ config/app.js
@@ -1,13 +1,13 @@
 var express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  cookieParser = require('cookie-parser'),
-  router = require('../routes/index'),
-  Estabelecimento = require('../controllers/estabelecimento'),
-  controller = require('../controllers/estabelecimento'),
-  compression = require("compression"),
-  helmet = require("helmet");
-path = require('path');
+    app = express(),
+    bodyParser = require("body-parser"),
+    cookieParser = require('cookie-parser'),
+    router = require('../routes/index'),
+    Estabelecimento = require('../controllers/estabelecimento'),
+    controller = require('../controllers/estabelecimento'),
+    compression = require("compression"),
+    helmet = require("helmet"),
+    path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -34,80 +34,80 @@ app.use('/hover', express.static('./node_modules/hover.css/css/'));
 app.use('/', router);
 
 app.get("/", function(req, res) {
-  res.render('index');
+    res.render('index');
 });
 
 app.get("/manual", function(req, res) {
-  res.sendFile(path.resolve('public/manual.pdf'));
+    res.sendFile(path.resolve('public/manual.pdf'));
 });
 
 app.get("/sobre", function(req, res) {
-  res.render('sobre');
+    res.render('sobre');
 });
 
 app.get("/listaCompleta", function(req, res) {
-  res.render('listaCompleta');
+    res.render('listaCompleta');
 });
 
 app.get("/ajuda", function(req, res) {
-  res.render('ajuda');
+    res.render('ajuda');
 });
 
 app.get("/privacidade", function(req, res) {
-  res.render('privacidade');
+    res.render('privacidade');
 });
 
 app.get("/ranking", function(req, res) {
-  var ranking = {};
-  controller.getAvaliacoes("presencaEquipe", function(rankingPresenca) {
-    ranking.presencaEquipe = rankingPresenca;
-    controller.getAvaliacoes("tempoEspera", function(rankingEspera) {
-      ranking.tempoEspera = rankingEspera;
-      controller.getAvaliacoes("qualidadeAtendimento", function(rankingAtendimento) {
-        ranking.qualidadeAtendimento = rankingAtendimento;
-        controller.getAvaliacoes("medicamentos", function(rankingMedicamentos) {
-          ranking.medicamentos = rankingMedicamentos;
-          controller.getAvaliacoes("equipamentos", function(rankingEquipamentos) {
-            ranking.equipamentos = rankingEquipamentos;
-            controller.getAvaliacoes("infraestrutura", function(rankingInfraestrutura) {
-              ranking.infraestrutura = rankingInfraestrutura;
-              res.json(ranking);
+    var ranking = {};
+    controller.getAvaliacoes("presencaEquipe", function(rankingPresenca) {
+        ranking.presencaEquipe = rankingPresenca;
+        controller.getAvaliacoes("tempoEspera", function(rankingEspera) {
+            ranking.tempoEspera = rankingEspera;
+            controller.getAvaliacoes("qualidadeAtendimento", function(rankingAtendimento) {
+                ranking.qualidadeAtendimento = rankingAtendimento;
+                controller.getAvaliacoes("medicamentos", function(rankingMedicamentos) {
+                    ranking.medicamentos = rankingMedicamentos;
+                    controller.getAvaliacoes("equipamentos", function(rankingEquipamentos) {
+                        ranking.equipamentos = rankingEquipamentos;
+                        controller.getAvaliacoes("infraestrutura", function(rankingInfraestrutura) {
+                            ranking.infraestrutura = rankingInfraestrutura;
+                            res.json(ranking);
+                        });
+                    });
+                });
             });
-          });
         });
-      });
     });
-  });
 });
 
-app.get("/ranking/:id", function(req,res) {
-  var nome;
-  if (req.params.id==="presencaEquipe") {
-    nome = "Presença de Profissionais de Saúde";
-  } else if (req.params.id==="tempoEspera") {
-    nome = "Tempo Espera";
-  } else if (req.params.id==="qualidadeAtendimento") {
-    nome = "Qualidade de Atendimento";
-  } else if (req.params.id==="medicamentos") {
-    nome = "Disponibilidade de Medicamentos";
-  } else if (req.params.id==="equipamentos") {
-    nome = "Disponibilidade de Equipamentos";
-  } else {
-    nome = "infraestrutura";
-  }
-  var order;
-  if(req.query.order === 'true') {
-    order = -1;
-  } else {
-    order = 1;
-  }
-  controller.getLista(req.params.id, order, req.query.cidade, function(ranking) {
-    res.render('listaCompleta', { data :  { ranking: ranking, nome: nome }});
-  });
+app.get("/ranking/:id", function(req, res) {
+    var nome;
+    if (req.params.id === "presencaEquipe") {
+        nome = "Presença de Profissionais de Saúde";
+    } else if (req.params.id === "tempoEspera") {
+        nome = "Tempo Espera";
+    } else if (req.params.id === "qualidadeAtendimento") {
+        nome = "Qualidade de Atendimento";
+    } else if (req.params.id === "medicamentos") {
+        nome = "Disponibilidade de Medicamentos";
+    } else if (req.params.id === "equipamentos") {
+        nome = "Disponibilidade de Equipamentos";
+    } else {
+        nome = "infraestrutura";
+    }
+    var order;
+    if (req.query.order === 'true') {
+        order = -1;
+    } else {
+        order = 1;
+    }
+    controller.getLista(req.params.id, order, req.query.cidade, function(ranking) {
+        res.render('listaCompleta', { data: { ranking: ranking, nome: nome } });
+    });
 });
 
 app.use(function(req, res, next) {
-  res.render('404');
+    res.render('404');
 });
 
 module.exports = app;
