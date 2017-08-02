fd474a531fcec3488792aba41df78a9ff8718458
yasser nascimento <yassern@protonmail.com>
2017-08-02 11:37:27 -0300

    datapicker no firefox

--- public/js/local.js
+++ public/js/local.js
@@ -4,5 +4,19 @@ $(document).ready(function(){
 });
 
 var now = moment;
-var now = now().tz('America/Recife').format('YYYY-MM-DD');
-$('input[type=date]').val(now);
+// Firefox
+if ( $('[type="date"]').prop('type') != 'date' ) {
+  $('[type="date"]').datepicker({
+      dateFormat: 'dd/mm/yy',
+      dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+      dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+      dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+      monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+      monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+    });
+  var now = now().tz('America/Recife').format('DD/MM/YYYY');
+  $('input[type=date]').val(now);
+} else {
+  var now = now().tz('America/Recife').format('YYYY-MM-DD');
+  $('input[type=date]').val(now);
+}
